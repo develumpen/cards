0f1e704eb9ca6e59ea867ae1ea33224eaf246635
develumpen <hi@develumpen.com>
2026-02-20 17:29:10 -0600

    refactor card actions

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,23 @@
 module CardsHelper
+  def card_actions(card)
+    return unless showing_current(card)
+    [ clickable(card), draggable(card), droppable(card) ].join(" ")
+  end
+
+  private
+    def showing_current(card)
+      params[:id].to_i != card.id
+    end
+
+    def clickable(card)
+      "click->card#goToCard"
+    end
+
+    def draggable(card)
+      "dragstart->card#dragStart dragover->card#dragOver"
+    end
+
+    def droppable(card)
+      "drop->card#drop"
+    end
 end
